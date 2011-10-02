f9e6fb4626fea2993c16599e55c34a55c945ec49
Jeremy Wright <jeremy@codestrokes.com>
2011-10-02 12:47:24 -0700

    Correctly multiplies a 3x3 matrix.

--- MatrixProj/main.cpp
+++ MatrixProj/main.cpp
@@ -7,6 +7,11 @@
  */
 #include <stdlib.h>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iterator>
+#include <string.h>
 
 using namespace std;
 
@@ -14,18 +19,26 @@ typedef int** Matrix;
 
 Matrix initMatrix(size_t size)
 {
-    Matrix M = new int*[3];
-    for(int i = 0; i < 3; ++i)
-       M[i] = new int[3];
+    Matrix M = new int*[size];
+    for(int i = 0; i < size; ++i)
+    {
+       M[i] = new int[size];
+       memset(M[i], 0, sizeof(int)*size);
+    }
     return M;
+}
+
+void freeMatrix(Matrix A, size_t size)
+{
+
 }
 
 void printMatrix(Matrix A, size_t size)
 {
-    for(int i = 0; i < 3; ++i)
+    for(int i = 0; i < size; ++i)
     {
         cout << "[";
-        for(int j=0;j < 3; ++j)
+        for(int j=0;j < size; ++j)
         {
             cout << " " << A[i][j];
         }
@@ -33,23 +46,96 @@ void printMatrix(Matrix A, size_t size)
     }
 }
 
-
-int main(int argc, const char *argv[])
+void loadMatrix(int* data, Matrix A, size_t size)
 {
-    Matrix A = initMatrix(3);
-    Matrix B = initMatrix(3);
-
-    Matrix C = initMatrix(3);
-
-    for(int i = 0; i < 3; ++i)
+    int k = 0;
+    for(int i = 0; i < size; ++i)
     {
-        for(int j=0;j < 3; ++j)
+        for(int j = 0; j < size; ++j)
         {
-            for(int k = 0; k < 3; ++k)
+            A[i][j] = data[k++];
+        }
+    }
+}
+
+vector<int> tokenize(string line)
+{
+    // construct a stream from the string
+	stringstream strstr(line);
+
+	// use stream iterators to copy the stream to the vector as whitespace separated strings
+	istream_iterator<int> it(strstr);
+	istream_iterator<int> end;
+	vector<int> results(it, end);
+
+	return results;
+}
+
+void parseInputFile(string filename, size_t size, Matrix& A, Matrix& B)
+{
+    ifstream fin(filename.c_str());
+    string line;
+    
+    for(int row = 0; row < size && !getline(fin, line).eof(); ++row)
+    {
+        vector<int> values = tokenize(line);
+        int col = 0;
+        for(vector<int>::iterator element = values.begin();
+               element != values.end();
+               ++element,  ++col)
+        {
+            A[row][col] = *element;
+        }
+    }
+    
+    for(int row = 0; row < size && !getline(fin, line).eof(); ++row)
+    {
+        vector<int> values = tokenize(line);
+        int col = 0;
+        for(vector<int>::iterator element = values.begin();
+               element != values.end();
+               ++element,  ++col)
+        {
+            B[row][col] = *element;
+        }
+    }
+}
+
+void MultiplyMatrix(Matrix const A, Matrix const B, Matrix& C, size_t size)
+{
+    for(int i = 0; i < size; ++i)
+    {
+        for(int j=0; j < size; ++j)
+        {
+            for(int k = 0; k < size; ++k)
                 C[i][j] += A[i][k] * B[k][j];
         }
     }
-    printMatrix(C, 3);
+
+}
+
+int main(int argc, const char *argv[])
+{
+
+    size_t size = atoi(argv[2]);
+    Matrix A = initMatrix(size);
+    Matrix B = initMatrix(size);
+
+    parseInputFile(string(argv[1]), size, A, B);
+
+    Matrix C = initMatrix(size);
+
+    printMatrix(A, size);
+    cout << endl << endl;
+    printMatrix(B, size);
+
+    MultiplyMatrix(A, B, C, size);
+    printMatrix(C, size);
+
+    freeMatrix(A, size);
+    freeMatrix(B, size);
+    freeMatrix(C, size);
+
 
 
 
